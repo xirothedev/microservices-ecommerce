7b5faa5ca693f55667bcd630554d4c6ba9b42b53
xirothedev <lethanhtrung.trungle@gmail.com>
2025-07-20 10:01:12 +0700

    Enhance CartController with add and remove product endpoints
    - Added POST endpoints for adding and removing products from the cart.
    - Updated CartController to include route decorators for better request handling.

--- backend/src/modules/cart/cart.controller.ts
+++ backend/src/modules/cart/cart.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { CartService } from './cart.service';
 import { AddProductCartDto } from './dto/add-product-cart.dto';
@@ -8,10 +8,12 @@ import { RemoveProductCartDto } from './dto/remove-product-cart.dto';
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
+  @Post('add')
   add(@Req() req: Request, @Body() body: AddProductCartDto) {
     return this.cartService.add(req, body);
   }
 
+  @Post('remove')
   remove(@Req() req: Request, @Body() body: RemoveProductCartDto) {
     return this.cartService.remove(req, body);
   }
